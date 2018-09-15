cd47066d993e835b4b87743411087abf3c8da770
Alexey Panteleychuk <alexeypanteleychuk@MacBook-Pro-Alexey.local>
2018-09-15 11:26:49 +0300

    panels used

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-export default class SidePanel extends React.Component {
+export default class Panel extends React.Component {
   render(){
     const { className } = this.props;
     return(
         <div className={className}>
+         {this.props.children}
         </div>
     );
   }
